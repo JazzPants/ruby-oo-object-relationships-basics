a3b26fe6ba3d1c4771709fb63a683114b351f830
JazzPants <leighton.l14@gmail.com>
2022-01-27 15:05:14 +1100

    Done! Added updates and notes

--- obj_relationship.rb
+++ obj_relationship.rb
@@ -1,26 +1,100 @@
+# class Song
+#     attr_accessor :title, :artist
+
+#     def initialize(title)
+#         @title = title
+#     end
+
+# end
+
+# class Artist
+#     attr_accessor :name, :genre
+#     def initialize(name,genre)
+#         @name=name
+#         @genre=genre
+
+#         @songs = []
+#     end
+
+#     def songs
+#         @songs
+#     end
+
+#     def add_song(song)
+#         @songs << song
+#         song.artist = self
+#     end
+# end
+
+#user can still make changes to artist be using song.artist = ... instead of using add_song
+# -> two sources of truth in two classes
+
+#FIX for above problem put both sources of truth in one class
 class Song
-    attr_accessor :title, :artist
+        attr_accessor :artist, :name
 
-    def initialize(title)
-        @title = title
+        @@all=[]
+    
+        def initialize(name,genre)
+            @name = name
+
+
+            @all << self
+        end
+
+        def self.all
+            @@all
+        end
+
+        #instead of using .artist.name, we can call this method (cleaner code)
+        def artist_name
+            self.artist.name
+        end
+    
     end
+    
+    class Artist
+        attr_accessor :name, :genre
+        def initialize(name,genre)
+            @name=name
+            @genre=genre
+        end
 
-end
+        def add_song(song)
+            song.artist = self
+        end
+#collaborating between two classes
+#artist class is collaborating with song class by using a method from the song class!
+        def songs 
+            Song.all.select {|song| song.artist == self}
+        end
 
-class Artist
-attr_accessor :name, :genre
-def initialize(name,genre)
-    @name=name
-    @genre=genre
-end
-end
+        def add_song_by_name(name)
+            song = Song.new(name)
+            song.artist = self
+          end
+    end
+    
 
-yesterday = Song.new("Yesterday")
+
+yesterday = Song.new("Yesterday", "Rock")
+letItBe = Song.new("Let It Be", "Rock")
 theBeatles = Artist.new("The beatles", "Rock")
 
+
+#can now use both these methods to add songs to the songs array
+theBeatles.add_song(yesterday)
+theBeatles.add_song(letItBe)
 #yesterday.artist = "The beatles"
 #change string value to object
+letItBe.artist = theBeatles
 yesterday.artist = theBeatles
+
+theBeatles.add_song_by_name("Yellow Submarine")
+
+#puts theBeatles.songs[0].title
+
+puts theBeatles.songs.inspect
 
 puts yesterday.artist
 puts yesterday.artist.genre
